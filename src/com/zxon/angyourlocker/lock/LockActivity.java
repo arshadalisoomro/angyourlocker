5e4e36cf20a4b9c3d0cf2af334dc3288317a7d11
liang <gmzhixingang@gmail.com>
2015-03-19 19:46:09 +0800

    init project

---
+++ src/com/zxon/angyourlocker/lock/LockActivity.java
@@ -0,0 +1,19 @@
+
+package com.zxon.angyourlocker.lock;
+
+import com.zxon.angyourlocker.R;
+import com.zxon.angyourlocker.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+    }
+}
